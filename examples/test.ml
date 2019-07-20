37ce1a2c47e213d454e8d0d48faa0e7be17403af
Romain Beauxis <toots@rastageeks.org>
2019-07-19 20:04:19 -0700

    Put something in test

--- examples/test.ml
+++ examples/test.ml
@@ -1,4 +1,11 @@
 open Srt
 
 let () =
-  setloglevel 1
+  startup ();
+  setloglevel 1;
+  let s = socket Unix.PF_INET Unix.SOCK_DGRAM 0 in 
+  Printf.printf "Messageapi: %b\n%!" (getsockflag s messageapi);
+  Printf.printf "Setting transtype to file..\n%!";
+  setsockflag s transtype `File;
+  Printf.printf "Messageapi: %b\n%!" (getsockflag s messageapi);
+  cleanup()
