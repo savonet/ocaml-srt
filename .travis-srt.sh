91ce770b5978e5cc6a0be705038a5ede5666a29a
Samuel Mimram <smimram@gmail.com>
2019-08-23 15:58:11 +0200

    Second try.

--- .travis-srt.sh
+++ .travis-srt.sh
@@ -1,6 +1,9 @@
 # Download and build srt
-sudo apt-get install tclsh pkg-config cmake libssl-dev build-essential
+
+sudo apt-get install tclsh pkg-config libssl-dev build-essential
+
 git clone https://github.com/Haivision/srt.git
+
 cd srt
 ./configure
 make
