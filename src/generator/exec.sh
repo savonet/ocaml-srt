0f46aeb7e27ec3b6614efb35cbaa8a29773c27af
Romain Beauxis <toots@rastageeks.org>
2024-11-25 23:27:53 -0600

    Only use wine64 when available.

--- src/generator/exec.sh
+++ src/generator/exec.sh
@@ -9,7 +9,11 @@ ARG3=$5
 if test "${SYSTEM}" = "mingw"; then
   OCAMLFIND_TOOLCHAIN=windows wine $CMD $ARG1 $ARG2 $ARG3
 elif test "${SYSTEM}" = "mingw64"; then
-  OCAMLFIND_TOOLCHAIN=windows wine64 $CMD $ARG1 $ARG2 $ARG3
+  if command -v wine64 > /dev/null 2>&1; then
+    OCAMLFIND_TOOLCHAIN=windows wine64 $CMD $ARG1 $ARG2 $ARG3
+  else
+    OCAMLFIND_TOOLCHAIN=windows wine $CMD $ARG1 $ARG2 $ARG3
+  fi
 else
   $CMD $ARG $ARG1 $ARG2 $ARG3
 fi
